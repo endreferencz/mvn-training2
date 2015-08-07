08dc3fa3aaeaa1355b7964aa6a1f51f9ec10db81
Endre Ferencz <Endre_Ferencz@epam.com>
2015-08-07 15:39:12 +0200

    initial commit

---
+++ src/main/java/com/epam/training/AppHu.java
@@ -0,0 +1,13 @@
+package com.epam.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppHu 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Helló Világ!" );
+    }
+}
